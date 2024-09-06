df5ea6f05838da086ee1d24ab54bc1bc8bb0ccec
Presiyana Borisova <84325049+PresiyanaBB@users.noreply.github.com>
2024-09-06 19:25:11 +0300

    Add files via upload

--- StackAndQueue/MyStack.hpp
+++ StackAndQueue/MyStack.hpp
@@ -1,165 +1,66 @@
+
 #include <iostream>
-#include <exception>
-template <typename T>
+
+template <typename T, const unsigned S>
 class MyStack
 {
-	T* data;
-	size_t capacity;
-	size_t size;
-
-	void resize();
-
-	void moveFrom(MyStack<T>&& other);
-	void copyFrom(const MyStack<T>& other);
-	void free();
+private:
+	T arr[S];
+	size_t size = 0;
 public:
-	MyStack();
 
-	MyStack(const MyStack<T>& other);
-	MyStack<T>& operator=(const MyStack<T>& other);
-
-	MyStack(MyStack<T>&& other);
-	MyStack<T>& operator=(MyStack<T>&& other);
-
-	void push(const T& obj);
+   	void push(const T& obj);
 	void push(T&& obj);
-	void pop();
 
 	const T& peek() const;
-	bool isEmpty() const;
+	void pop();
 
-	~MyStack();
+	bool isEmpty() const;
+        bool isFull() const;
 };
 
-template <typename T>
-MyStack<T>::MyStack()
+
+template <typename T, const unsigned S>
+void MyStack<T,S>::push(const T& obj)
 {
-	capacity = 4;
-	data = new T[capacity];
-	size = 0;
+	if (size >= S)
+		return;
+	arr[size++] = obj;
 }
 
-template <typename T>
-void MyStack<T>::push(const T& obj)
+template <typename T, const unsigned S>
+void MyStack<T,S>::push(T&& obj)
 {
-	if (size == capacity)
-		resize();
-
-	data[size++] = obj; //operator=
+	if (size >= S)
+		return;
+	arr[size++] = std::move(obj);
 }
 
-template <typename T>
-void MyStack<T>::push(T&& obj)
+template <typename T, const unsigned S>
+const T& MyStack<T,S>::peek() const
 {
-	if (size == capacity)
-		resize();
+	if (isEmpty())
+		throw std::out_of_range("Error!");
 
-	data[size++] = std::move(obj); //move operator =
+	return arr[size - 1];
 }
 
-template <typename T>
-bool MyStack<T>::isEmpty() const
+template <typename T, const unsigned S>
+void MyStack<T,S>::pop()
+{
+	if (isEmpty())
+		throw std::out_of_range("Error!");
+	size--;
+}
+
+template <typename T, const unsigned S>
+bool MyStack<T,S>::isEmpty() const
 {
 	return size == 0;
 }
 
-template <typename T>
-const T& MyStack<T>::peek() const
+template <typename T, const unsigned S>
+bool MyStack<T,S>::isFull() const
 {
-	if (isEmpty())
-		throw std::logic_error("Empty stack!");
-
-	return data[size - 1];
-}
-
-template <typename T>
-void MyStack<T>::pop()
-{
-	if (isEmpty())
-		throw std::logic_error("Empty stack!");
-	
-	size--;
-}
-
-template <typename T>
-void MyStack<T>::resize()
-{
-	T* resizedData = new T[capacity * 2];
-	for (size_t i = 0; i < size; i++)
-		resizedData[i] = data[i];
-
-	capacity *= 2;
-	delete[] data;
-	data = resizedData;
-}
-
-
-template <typename T>
-void MyStack<T>::copyFrom(const MyStack<T>& other)
-{
-	data = new T[other.capacity];
-	for (int i = 0; i < other.capacity; i++)
-		data[i] = other.data[i];
-
-	size = other.size;
-	capacity = other.capacity;
-}
-
-template <typename T>
-void MyStack<T>::moveFrom(MyStack<T>&& other)
-{
-	size = other.size;
-	capacity = other.capacity;
-
-	data = other.data;
-	other.data = nullptr;
-
-	other.size = other.capacity = 0;
-}
-
-template <typename T>
-void MyStack<T>::free()
-{
-	delete[] data;
-}
-
-template <typename T>
-MyStack<T>::MyStack(const MyStack<T>& other)
-{
-	copyFrom(other);
-}
-
-template <typename T>
-MyStack<T>& MyStack<T>::operator=(const MyStack<T>& other)
-{
-	if (this != &other)
-	{
-		free();
-		copyFrom(other);
-	}
-	return *this;
-}
-
-template <typename T>
-MyStack<T>::MyStack(MyStack<T>&& other)
-{
-	moveFrom(std::move(other));
-}
-
-template <typename T>
-MyStack<T>& MyStack<T>::operator=(MyStack<T>&& other)
-{
-	if (this != &other)
-	{
-		free();
-		moveFrom(std::move(other));
-	}
-	return *this;
-}
-
-
-template <typename T>
-MyStack<T>::~MyStack()
-{
-	free();
+	return size == S;
 }
